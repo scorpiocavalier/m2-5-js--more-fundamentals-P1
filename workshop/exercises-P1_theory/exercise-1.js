3f7dd1b1cb85712da2b37a5b812d02280eed5db0
Arturo Chen <art.can.fly@gmail.com>
2020-06-22 10:28:21 -0400

    E1 done

--- workshop/exercises-P1_theory/exercise-1.js
+++ workshop/exercises-P1_theory/exercise-1.js
@@ -1,32 +1,35 @@
 // Given the following functions, answer the questions below.
+const square = x => x * x;
 
-const square = (x) => {
-  return x * x;
-};
+const decrement = x => x - 1;
 
-const decrement = (x) => {
-  return x - 1;
-};
+const duplicateString = x => x.concat(x);
 
-const duplicateString = (x) => {
-  return x.concat(x);
-};
-
-const reverseString = (str) => {
-  const splitString = str.split(''); // var splitString = "hello".split("");
-  const reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-  return reverseArray.join(''); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-};
+const reverseString = str => str.split('').reverse().join('');
 
 // Expand each of the following and get the result of the expression
+
 // 1-1
 square(decrement(square(decrement(3))));
+// decrement(3) => 2
+// square(2) => 4
+// decrement(4) => 3
+// square(3) => 9
 
 // 1-2
 decrement(decrement(square(square(3))));
+// square(3) => 9
+// square(9) => 81
+// decrement(81) => 80
+// decrement(80) => 79
 
 // 1-3
 duplicateString(reverseString('hello'));
+// reverseString('hello') => ['h', 'e', 'l', 'l', 'o'] => ['o', l', 'l', 'e', 'h'] => 'olleh'
+// duplicateString('olleh') => 'olleholleh'
 
 // 1-4
 reverseString(duplicateString(duplicateString('foo')));
+// duplicateString('foo') => 'foofoo'
+// duplicateString('foofoo') => 'foofoofoofoo'
+// reverseString('foofoofoofoo') => 'oofoofoofoof'
