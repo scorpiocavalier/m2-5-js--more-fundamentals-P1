44fa24d0f7a4387f1a48899fed92198758cc8743
Arturo Chen <art.can.fly@gmail.com>
2020-06-23 00:41:30 -0400

    E3.1 done

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,31 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+
+const handleClick = index => {
+    switch(index) {
+        case 1:
+            event.target.style.opacity = '0';
+            break;
+        case 2:
+            event.target.style.backgroundColor = 'crimson';
+            break;
+        case 3:
+            event.target.style.backgroundColor = 'lightblue';
+            break;
+        case 4:
+            event.target.classList.add('jitters');
+        default: 
+            break;
+    }
+}
+
+const app = () => {
+    
+    for(let i = 1; i <= 4; i++) {
+        let btn = document.querySelector(`#btn-${i}`);
+        btn.addEventListener('click', () => handleClick(i));
+    }
+}
+
+app();
