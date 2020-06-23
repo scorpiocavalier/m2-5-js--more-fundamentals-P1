5a5bd79f0c77289c694eac4c76c83c1cfbec1185
Arturo Chen <art.can.fly@gmail.com>
2020-06-23 10:09:34 -0400

    E3.4 in progress

--- workshop/exercises-P3_problems/exercise-3-4/app.js
+++ workshop/exercises-P3_problems/exercise-3-4/app.js
@@ -1,2 +1,18 @@
 // Exercise 3-4 - The GRID
 // ------------
+let board = document.querySelector('#board');
+
+for(let r=0; r<10; r++) {
+    for(let c=1; c<=10; c++) {
+        let square = document.createElement('button');
+        square.classList.add('cell');
+        let row = r;
+        if(col > 9) {
+            row += c / 10;
+            // col = 
+        }
+        let id = r * 10 + col;
+        square.id = `cell-${id}`;
+        board.appendChild(square);
+    }
+}
