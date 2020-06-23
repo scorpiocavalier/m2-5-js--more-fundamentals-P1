0cd56f316f542b056cacb9bdbe1d8c6f62d4f52d
Arturo Chen <art.can.fly@gmail.com>
2020-06-22 22:20:55 -0400

    E2.7 done

--- workshop/exercises-P2_tests/exercise-2-7.js
+++ workshop/exercises-P2_tests/exercise-2-7.js
@@ -2,7 +2,7 @@
 // ------------
 
 // Step 1
-// - The function input is an array.
+// - The function's parameter is an array.
 // - The first element of the array is a string. The second is a number.
 // - Make this function return the string repeated as many times as specified by the second element of the array.
 // - If a negative number or zero is specified, return an empty string. If any invalid parameters are supplied return undefined.
@@ -12,11 +12,31 @@
 // f(["foo", -1]) // ""
 
 function repeat(arr) {
-  // Your code here
+  let [aString, aNumber] = arr;
+
+  if(!Array.isArray(arr)
+    || typeof(aString) !== 'string'
+    || !Number.isInteger(aNumber))
+    return undefined;
+
+  let repeatedString = '';
+  
+  if(aNumber > 0)
+    for(let i=0; i<aNumber; i++)
+      repeatedString = repeatedString.concat(aString);
+  
+  return repeatedString;
 }
 
 // We need 7 test cases.
 // Don't forget to test all of the question parameters
+expect(repeat(['foo', 3]), 'foofoofoo');
+expect(repeat(['foo', 0]), '');
+expect(repeat(['foo', -3]), '');
+expect(repeat([123, -3]), undefined);
+expect(repeat(['foo', 'abc']), undefined);
+expect(repeat('foo'), undefined);
+expect(repeat('f2'), undefined);
 
 /**
  * -------------------------------------------------------------------
