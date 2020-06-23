8c86c0b0fdae47f52fdb28ef4ff3a56d4546d6b7
Arturo Chen <art.can.fly@gmail.com>
2020-06-23 02:44:54 -0400

    E3.3 done

--- workshop/exercises-P3_problems/exercise-3-3/app.js
+++ workshop/exercises-P3_problems/exercise-3-3/app.js
@@ -5,3 +5,70 @@
 
 // HINT:
 // to remove the 'jitters' class, check the length of the classList.
+
+const hasGoldClass = btn => btn.classList == 'gold';
+
+const addGoldClass = btn => btn.classList = 'gold';
+
+const removeGoldClass = btn => btn.classList = '';
+
+const toggleColor = (btn, color) => btn.style.backgroundColor = (color == 'gold') ? 'gold' : color;
+
+
+const handleClick = index => {
+    
+    let btn = event.target;
+    let golden = hasGoldClass(btn);
+    
+    if(golden) 
+        removeGoldClass(btn);
+    else {
+        addGoldClass(btn);
+        toggleColor(btn, 'gold');
+    }
+    
+    switch(index) {
+        case 1: 
+            btn.style.opacity = golden ? '0' : '1';
+            break;
+        case 2:
+            golden && toggleColor(btn, 'crimson');
+            break;
+        case 3: 
+            golden && toggleColor(btn, 'lightblue');
+            break;
+        case 4:
+            btn.classList = golden && 'jitters';
+            break;
+        default: 
+            break;
+    }
+}
+
+const handleReset = () => {
+
+    for(let i = 1; i <= 4; i++) {
+        let btn = document.querySelector(`#btn-${i}`);
+
+        addGoldClass(btn);
+        toggleColor(btn, 'gold');
+
+        if(i == 1)
+            btn.style.opacity = '1';
+    }
+}
+
+const app = () => {
+
+    let reset_btn = document.querySelector('#reset');
+    reset_btn.addEventListener('click', handleReset);
+
+    for(let i = 1; i <= 4; i++) {
+        let btn = document.querySelector(`#btn-${i}`);
+        addGoldClass(btn);
+        toggleColor(btn, 'gold');
+        btn.addEventListener('click', () => handleClick(i));
+    }
+}
+
+app();
