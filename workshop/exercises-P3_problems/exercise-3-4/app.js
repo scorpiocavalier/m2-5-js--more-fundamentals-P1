edbccb2cd46d9c766be1c97db5f9229e726efab2
Arturo Chen <art.can.fly@gmail.com>
2020-06-25 21:31:40 -0400

    E3.4 done

--- workshop/exercises-P3_problems/exercise-3-4/app.js
+++ workshop/exercises-P3_problems/exercise-3-4/app.js
@@ -1,18 +1,21 @@
 // Exercise 3-4 - The GRID
 // ------------
-let board = document.querySelector('#board');
+const SQUARE_SIDE = '60px'
+const ROWS = 10
+const COLUMNS = 10
 
-for(let r=0; r<10; r++) {
-    for(let c=1; c<=10; c++) {
-        let square = document.createElement('button');
-        square.classList.add('cell');
-        let row = r;
-        if(col > 9) {
-            row += c / 10;
-            // col = 
-        }
-        let id = r * 10 + col;
-        square.id = `cell-${id}`;
-        board.appendChild(square);
+let board = document.querySelector('#board')
+board.style.gridTemplateRows = `repeat(${ROWS}, ${SQUARE_SIDE})`
+board.style.gridTemplateColumns = `repeat(${COLUMNS}, ${SQUARE_SIDE})`
+
+for(let r=0; r<ROWS; r++) {
+    for(let c=1; c<=COLUMNS; c++) {
+        let square = document.createElement('button')
+        square.id = `cell-${10 * r + c}`
+        // square.innerText = square.id
+        square.classList.add('cell')
+        square.style.border = 'none'
+        square.style.outline = 'none'        
+        board.appendChild(square)
     }
 }
